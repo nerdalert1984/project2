ed9aa409e77b7dc5e69579e633745e183ed12478
Matt Ballert <nerdalert1984@Matts-MacBook-Pro.local>
2012-10-05 13:17:51 -0400

    Added initial variables.

--- js/rename-me.js
+++ js/rename-me.js
@@ -3,3 +3,18 @@
 // Project 2
 // Advanced JavaScript
 
+// My Variables
+var heroName = "Hunter, Slayer of Evil ",
+	heroPresent = true,
+	someMonsters = true,
+	numOfMonsters = 4,
+	monsters = [ " Hunpar the Goblin ", 
+				 " Wretchmaw the Troll King ", 
+				 " Arch-Lich Volsonis ",
+				 " Firereek the Red Dragon "
+			   ],
+	heroicWeapons = [ " Steel shortsword ",
+					  " Longsword of Eternal Flame ",
+					  " Blessed Mace of Smiting ",
+					  " Greatsword of Draconic Ruin "];
+	
