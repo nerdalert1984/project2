16937832e37a7d00b25e150803ff3fd05203b215
Matt Ballert <nerdalert1984@Matts-MacBook-Pro.local>
2012-10-06 20:56:25 -0400

    Added procedures and finished project.

--- js/rename-me.js
+++ js/rename-me.js
@@ -1,5 +1,5 @@
 // Author: Matt Ballert
-// 10/4/2012
+// 10/6/2012
 // Project 2
 // Advanced JavaScript
 
@@ -13,11 +13,14 @@ var nameOfHero = "Hunter, Bane of Chaos ",
 				 " Arch-Lich Volsonis ",
 				 " Firereek the Red Dragon "
 ],
-	weapons = [ " Steel shortsword ",
-					  " Longsword of Eternal Flame ",
-					  " Blessed Mace of Smiting ",
-					  " Greatsword of Draconic Ruin "
-]
+	heroWeapon = "the Twin Blades of Frost and Flame"
+ 
+//Procedure (Function with no return value)
+var startTale = function (villageName) {
+	console.log("The tale begins with " + nameOfHero + "arriving in the small village of " + villageName + ".");
+};
+
+startTale("Dunhollow");
 	
 // Boolean Function with return.
 var findHero = function (heroName) {
@@ -30,7 +33,7 @@ var findHero = function (heroName) {
 var haveHero = findHero("Hunter the Slayer of Evil");
 console.log(haveHero);
 
-// Number Function + While Loop (Counts down number of monsters in need of slaying.)
+// Number Function + While Loop with return (Counts up number of villagers pleading)
 var pleadingVillagers = function (numOfVillagers) {
 while (numOfVillagers <= 4) {
 	enoughVillagers = "There are " + numOfVillagers + " villagers pleading for help.";
@@ -58,11 +61,19 @@ console.log(heSays);
 // For Loop Inside of a Function
 var showdown = function (monsterName) {
 for (m = monsterName; m < 4; m++) {
-	heroFight = nameOfHero + "has fought and slain " + monsters[monsterName] + ".";
+	heroFight = nameOfHero + "has fought and slain " + monsters[m] + ".";
 	console.log(heroFight);
 };
 	return heroFight;
 };
 
 var whatShowdown = showdown(0)
-	
+
+//Procedure (Function with no return value)
+var endTale = function (villageName) {
+	console.log(nameOfHero + "has saved the village of " + villageName + "!" + " Our hero sheathes ");
+	console.log(heroWeapon + " and begins his journey to the next town in need of his help.");
+};
+
+//Procedure (Function with no return value)
+endTale("Dunhollow");
