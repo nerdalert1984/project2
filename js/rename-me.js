f47808fbddde9b29d2c4efd7ad41f5fbe6d044e3
Matt Ballert <nerdalert1984@Matts-MacBook-Pro.local>
2012-10-06 20:14:41 -0400

    Add string function, started for loop.

--- js/rename-me.js
+++ js/rename-me.js
@@ -4,7 +4,8 @@
 // Advanced JavaScript
 
 // My Variables
-var heroPresent = true,
+var nameOfHero = "Hunter, Bane of Chaos ",
+	heroPresent = true,
 	haveWeapons = true,
 	numOfMonsters = 4,
 	monsters = [ " Hunpar the Goblin ", 
@@ -12,7 +13,7 @@ var heroPresent = true,
 				 " Arch-Lich Volsonis ",
 				 " Firereek the Red Dragon "
 ],
-	heroicWeapons = [ " Steel shortsword ",
+	weapons = [ " Steel shortsword ",
 					  " Longsword of Eternal Flame ",
 					  " Blessed Mace of Smiting ",
 					  " Greatsword of Draconic Ruin "
@@ -37,9 +38,31 @@ while (numOfVillagers <= 4) {
 	numOfVillagers++;
 
 };
-	return enoughVillagers;};
+	return enoughVillagers;
+};
+
 var pleaVillagers = pleadingVillagers(0)
-console.log(pleaVillagers)
+
 
 console.log("There are so many villagers pleading for help.")
 
+// String Function with Return
+var saysHelp = function (whoSays) {
+	huntSays = whoSays + ' says, "I will help you with your monster problem!"';
+	return huntSays;
+};
+
+var heSays = saysHelp("Hunter");
+console.log(heSays);
+
+// For Loop Inside of a Function
+var showdown = function (monsterName) {
+for (m = monsterName; m < 4; m++) {
+	heroFight = nameOfHero + "has fought and slain " + monsters[monsterName] + ".";
+	console.log(heroFight);
+};
+	return heroFight;
+};
+
+var whatShowdown = showdown(0)
+	
